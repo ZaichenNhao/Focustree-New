6586e8bec879faead0148d537ae6f8c79e0f640c
Zaichen <sunny.yuan0333@gmail.com>
2020-08-11 18:48:33 -0700

    Started working on the Login page, the login w/ google is overflowing

---
+++ lib/helper/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const svgLogoBlack = 'assets/svg/logoBlack.svg';
+const svgLogoTextBlack = 'assets/svg/logowithtextblack.svg';
+const svgGoogle = 'assets/svg/google.svg';
